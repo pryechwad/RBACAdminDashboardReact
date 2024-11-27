1f92029ef60ce776bc16325b992341fe7f59cd1d
Prathmesh Yechwad <89580407+pryechwad@users.noreply.github.com>
2024-11-27 23:07:08 +0530

    Add files via upload
    add new changes in app.jsx file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#43", // Indigo for buttons and headers
+        secondary: "#F3F4F6", // Light gray for backgrounds
+      },
+    },
+  },
+  plugins: [],
+}
